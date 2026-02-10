48bf23a257048ef69b61ad511b35305ea33962ba
OLIVIOU1224 <164617023+OLIVIOU1224@users.noreply.github.com>
2026-02-10 14:22:17 +0900

    Update zmk_ui.c

--- boards/shields/Fern/zmk_ui.c
+++ boards/shields/Fern/zmk_ui.c
@@ -5,7 +5,6 @@
 #include <zmk/display/widgets/peripheral_battery_status.h>
 #include <lvgl.h>
 
-// 위젯 객체 선언
 static struct zmk_widget_layer_status layer_status_widget;
 static struct zmk_widget_output_status output_status_widget;
 static struct zmk_widget_peripheral_battery_status peripheral_battery_status_widget;
@@ -14,18 +13,17 @@ lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen;
     screen = lv_obj_create(NULL);
 
-    // 1. 레이어 표시 (맨 위)
+    // 1. 레이어 (맨 위)
     zmk_widget_layer_status_init(&layer_status_widget, screen);
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_MID, 0, 2);
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_MID, 0, 0);
 
-    // 2. 출력/블루투스 상태 (레이어 바로 아래)
+    // 2. 연결 상태 (중간)
     zmk_widget_output_status_init(&output_status_widget, screen);
-    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_MID, 0, 25);
+    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_CENTER, 0, -10);
 
-    // 3. 좌/우 배터리 상태 (맨 아래)
-    // ZMK의 peripheral_battery_widget은 동글에 연결된 모든 주변장치를 리스트로 보여줍니다.
+    // 3. 좌우 배터리 (맨 아래) - 동글 전용 위젯
     zmk_widget_peripheral_battery_status_init(&peripheral_battery_status_widget, screen);
-    lv_obj_align(zmk_widget_peripheral_battery_status_obj(&peripheral_battery_status_widget), LV_ALIGN_BOTTOM_MID, 0, -5);
+    lv_obj_align(zmk_widget_peripheral_battery_status_obj(&peripheral_battery_status_widget), LV_ALIGN_BOTTOM_MID, 0, 0);
 
     return screen;
 }
