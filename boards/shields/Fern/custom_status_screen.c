3da4cd166c30b9a4d6e0ea2158a9c4935d3d93a8
OLIVIOU1224 <164617023+OLIVIOU1224@users.noreply.github.com>
2026-02-11 19:17:43 +0900

    Update custom_status_screen.c

--- boards/shields/Fern/custom_status_screen.c
+++ boards/shields/Fern/custom_status_screen.c
@@ -1,3 +1,4 @@
+
 /*
  * Copyright (c) 2024 The ZMK Contributors
  *
@@ -10,54 +11,89 @@
 #include "widgets/bongo_cat.h"
 #include "widgets/layer_status.h"
 #include "widgets/output_status.h"
+#include "widgets/hid_indicators.h"
+#include "widgets/wpm_status.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static struct zmk_widget_output_status output_status_widget;
-static struct zmk_widget_layer_status layer_status_widget;
-static struct zmk_widget_dongle_battery_status dongle_battery_status_widget;
-static struct zmk_widget_modifiers modifiers_widget;
-static struct zmk_widget_bongo_cat bongo_cat_widget;
 
-// 스타일은 static으로 선언하여 메모리 재할당을 방지합니다.
-static lv_style_t global_style;
-static bool style_initialized = false;
+#if IS_ENABLED(CONFIG_ZMK_BATTERY)
+static struct zmk_widget_dongle_battery_status dongle_battery_status_widget;
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_LAYER)
+static struct zmk_widget_layer_status layer_status_widget;
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_MODIFIERS)
+static struct zmk_widget_modifiers modifiers_widget;
+#if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
+static struct zmk_widget_hid_indicators hid_indicators_widget;
+#endif
+
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_BONGO_CAT)
+static struct zmk_widget_bongo_cat bongo_cat_widget;
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_WPM)
+static struct zmk_widget_wpm_status wpm_status_widget;
+#endif
+
+lv_style_t global_style;
 
 lv_obj_t *zmk_display_status_screen() {
     lv_obj_t *screen;
+
     screen = lv_obj_create(NULL);
 
-    // 스타일 중복 초기화 방지
-    if (!style_initialized) {
-        lv_style_init(&global_style);
-        lv_style_set_text_font(&global_style, &lv_font_unscii_8);
-        lv_style_set_text_letter_space(&global_style, 1);
-        lv_style_set_text_line_space(&global_style, 1);
-        style_initialized = true;
-    }
-    
+    lv_style_init(&global_style);
+    lv_style_set_bg_color(&global_style, lv_color_white());
+    lv_style_set_bg_opa(&global_style, LV_OPA_COVER);
+    lv_style_set_text_color(&global_style, lv_color_black());
+    lv_style_set_text_font(&global_style, &lv_font_unscii_8);
+    lv_style_set_text_letter_space(&global_style, 1);
+    lv_style_set_text_line_space(&global_style, 1);
     lv_obj_add_style(screen, &global_style, LV_PART_MAIN);
     
-    // 1. 레이어 상태 (좌상단) - 오타 수정됨
-    zmk_widget_layer_status_init(&layer_status_widget, screen);
-    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+    zmk_widget_output_status_init(&output_status_widget, screen);
+    lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
 
-    // 2. 출력 상태 (BLE/USB 상태 - 필요시 주석 해제)
-    // zmk_widget_output_status_init(&output_status_widget, screen);
-    // lv_obj_align(zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_TOP_RIGHT, 0, 0);
+#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_WPM)
+    zmk_widget_wpm_status_init(&wpm_status_widget, screen);
+    lv_obj_align_to(zmk_widget_wpm_status_obj(&wpm_status_widget), zmk_widget_output_status_obj(&output_status_widget), LV_ALIGN_OUT_RIGHT_MID, 7, 0);
+#endif
 
-    // 3. 봉고캣 (중앙 우측) - 0.91인치 높이를 고려해 y좌표 조정
+#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_BONGO_CAT)
     zmk_widget_bongo_cat_init(&bongo_cat_widget, screen);
-    lv_obj_align(zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_RIGHT, 0, 0);
+    lv_obj_align(zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_RIGHT, 0, -7);
+#endif
 
-    // 4. 모디파이어 상태 (좌하단)
+#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_MODIFIERS)
     zmk_widget_modifiers_init(&modifiers_widget, screen);
     lv_obj_align(zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_BOTTOM_LEFT, 0, 0);
+#if IS_ENABLED(CONFIG_ZMK_HID_INDICATORS)
+    zmk_widget_hid_indicators_init(&hid_indicators_widget, screen);
+    lv_obj_align_to(zmk_widget_hid_indicators_obj(&hid_indicators_widget), zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_OUT_TOP_LEFT, 0, -2);
+#endif
+#endif
 
-    // 5. 배터리 상태 (우하단)
+#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_LAYER)
+    zmk_widget_layer_status_init(&layer_status_widget, screen);
+#if IS_ENABLED(CONFIG_ZMK_DONGLE_DISPLAY_BONGO_CAT)
+    lv_obj_align_to(zmk_widget_layer_status_obj(&layer_status_widget), zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_RIGHT, 0, 5);
+#else
+    lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_RIGHT, 0, -3);
+#endif
+#endif
+
+#if IS_ENABLED(CONFIG_ZMK_BATTERY)
     zmk_widget_dongle_battery_status_init(&dongle_battery_status_widget, screen);
-    lv_obj_align(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget), LV_ALIGN_BOTTOM_RIGHT, 0, 0);
-    
+    lv_obj_align(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget), LV_ALIGN_TOP_RIGHT, 0, 0);
+#endif
+
     return screen;
 }
